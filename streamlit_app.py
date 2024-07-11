9f457eff6fb66084f8ee3207266df57388534391
lucassantoscsa <150693349+lucassantoscsa@users.noreply.github.com>
2024-07-11 16:47:39 +0000

    modified:   requirements.txt 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import datetime
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 **My new a**pp po")
+sono = st.time_input("Tempo de sono", value = datetime.time(0, 0))
+tSono = sono.hour*60+sono.minute
+tela = st.time_input("Tempo de tela", value = datetime.time(0, 0))
+tTela = tela.hour*60+tela.minute
+escola = st.time_input("Tempo de escola", value = datetime.time(0, 0))
+tEscola = escola.hour*60+escola.minute
+fisico = st.time_input("Tempo de Atividade Física", value = datetime.time(0, 0))
+tFisico = fisico.hour*60+fisico.minute
+
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
