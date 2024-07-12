03903e7c47e3e7e12f797da5a6bc5e6699da1113
lucassantoscsa <150693349+lucassantoscsa@users.noreply.github.com>
2024-07-12 13:35:34 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with st.expander("Ver resultado"):
                         labels = 'Sono', 'Escola', 'Atividades Extras', 'Tela', 'Estudos', 'Refeição e Higiente Pessoal','Livre'
                         sizes = [tSono, tEscola, tFisico, tTela,testudo, tRefeicao, livre]                        
                         fig1, ax1 = plt.subplots()
-                        ax1.pie(sizes, wedgeprops={"linewidth": 1, "edgecolor": "white","width":0.4},colors = colors,labels=labels, autopct='%1.1f%%', shadow=False, startangle=90,textprops={"color":"#fff","fontsize":"large"})
+                        ax1.pie(sizes, wedgeprops={"linewidth": 1, "edgecolor": "white","width":0.1},colors = colors,labels=labels, autopct='%1.1f%%', shadow=False, startangle=90,textprops={"color":"#fff","fontsize":"large"})
                         ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
                         fig1.set_facecolor('#0e1117')
                         st.pyplot(fig1)
@@ -42,7 +42,7 @@ with st.expander("Ver resultado"):
                         labels = 'Sono', 'Escola', 'Atividades Extras', 'Tela', 'Estudos', 'Refeição e Higiente Pessoal','Livre'
                         sizes = [tSono, tEscola, tFisico, tTela,testudo, tRefeicao,livre]
                         fig2, ax2 = plt.subplots()
-                        ax2.pie(sizes, wedgeprops={"linewidth": 1, "edgecolor": "white","width":0.4},colors = colors,labels=labels, autopct='%1.1f%%', shadow=False, startangle=90,textprops={"color":"#fff","fontsize":"larger"})
+                        ax2.pie(sizes, wedgeprops={"linewidth": 1, "edgecolor": "white","width":0.1},colors = colors,labels=labels, autopct='%1.1f%%', shadow=False, startangle=90,textprops={"color":"#fff","fontsize":"larger"})
                         ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
                         fig2.set_facecolor('#0e1117')
                         st.pyplot(fig2)
@@ -51,7 +51,7 @@ with st.expander("Ver resultado"):
                         labels = '', 'Tempo Excedente Gasto em Tela: '+str(horasNovo)+" horas."
                         sizes = [24*60-tTelaAux, tTelaAux]
                         fig3, ax3 = plt.subplots()
-                        ax3.pie(sizes, wedgeprops={"linewidth": 1, "edgecolor": "white","width":0.4},colors = colors,labels=labels, autopct='%1.1f%%', shadow=False, startangle=90, textprops={"color":"#fff","fontsize":"larger"})
+                        ax3.pie(sizes, wedgeprops={"linewidth": 1, "edgecolor": "white","width":0.1},colors = colors,labels=labels, autopct='%1.1f%%', shadow=False, startangle=90, textprops={"color":"#fff","fontsize":"larger"})
                         ax3.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
                         fig3.set_facecolor('#0e1117')
                         st.pyplot(fig3)
