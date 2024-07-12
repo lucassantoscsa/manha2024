6768d17a70290a2bd61c8e60766428b8561d6742
lucassantoscsa <150693349+lucassantoscsa@users.noreply.github.com>
2024-07-12 11:24:05 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,36 +19,33 @@ with st.expander("Ver resultado"):
         soma = sum(tamanhos)
         livre = 0
         if soma < 24*60:
-                col1, col2 = st.columns(2)
-                with col1:   
-                        livre = 24*60-soma
-                        labels = 'Sono', 'Escola', 'Físico', 'Tela', 'Estudos','Livre'
-                        sizes = [tSono, tEscola, tFisico, tTela,testudo, livre]
-                        fig1, ax1 = plt.subplots()
-                        ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
-                        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-                        st.pyplot(fig1)
+                livre = 24*60-soma
+                labels = 'Sono', 'Escola', 'Físico', 'Tela', 'Estudos','Livre'
+                sizes = [tSono, tEscola, tFisico, tTela,testudo, livre]
+                fig1, ax1 = plt.subplots()
+                ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
+                ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+                st.pyplot(fig1)
         else:
-                col1, col2 = st.columns(2)
-                with col1:
-                        st.title('Dia 1')
-                        tTelaAux = tTela
-                        tTela = 24*60-(soma-tTela)
-                        tTelaAux = tTelaAux-tTela
-                        labels = 'Sono', 'Escola', 'Físico', 'Tela', 'Estudos','Livre'
-                        sizes = [tSono, tEscola, tFisico, tTela,testudo, livre]
-                        fig2, ax2 = plt.subplots()
-                        ax2.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
-                        ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-                        st.pyplot(fig2)
-                with col2:
-                        st.title('Dia 2')
-                        st.write("Com os dados apresentados, seria necessário mais um segundo dia em que você usaria 3 horas usando o celular, será que você tem cumprido mesmo os horários que propôs acima?")
-                        labels = '', 'Tela'
-                        sizes = [24*60-tTelaAux, tTelaAux]
-                        fig3, ax3 = plt.subplots()
-                        ax3.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
-                        ax3.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-                        st.pyplot(fig3)
+                st.title('Dia 1')
+                tTelaAux = tTela
+                tTela = 24*60-(soma-tTela)
+                tTelaAux = tTelaAux-tTela
+                labels = 'Sono', 'Escola', 'Físico', 'Tela', 'Estudos','Livre'
+                sizes = [tSono, tEscola, tFisico, tTela,testudo, livre]
+                fig2, ax2 = plt.subplots()
+                ax2.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
+                ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+                st.pyplot(fig2)
+                st.title('Dia 2')
+                st.write("Com os dados apresentados, seria necessário mais um segundo dia em que você usaria 3 horas usando o celular, será que você tem cumprido mesmo os horários que propôs acima?")
+                labels = '', 'Tela'
+                sizes = [24*60-tTelaAux, tTelaAux]
+                fig3, ax3 = plt.subplots()
+                ax3.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
+                ax3.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+                st.pyplot(fig3)
