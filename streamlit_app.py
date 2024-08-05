c8459d472ec6f7cc62efdc4bc2f6207485851fef
lucassantoscsa <150693349+lucassantoscsa@users.noreply.github.com>
2024-08-05 11:48:55 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,5 @@ st.set_page_config(
     page_icon="🔵",
 )
 st.image("https://www.colegiosantoantonio.com.br/wp-content/uploads/2024/06/csa-115-anos-1.png")
+st.link_button("Rotina de Estudos", "https://manhacsa2024.streamlit.app/Rotina_de_Estudos")
+st.link_button("Tempo de tela", "https://manhacsa2024.streamlit.app/Tempo_de_Tela")
