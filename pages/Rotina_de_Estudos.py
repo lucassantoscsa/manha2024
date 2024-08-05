c861f216ad334f8370defb33fcaea8aa7f5d1b40
lucassantoscsa <150693349+lucassantoscsa@users.noreply.github.com>
2024-08-05 12:05:21 +0000

    modified:   pages/Rotina_de_Estudos.py

--- pages/Rotina_de_Estudos.py
+++ pages/Rotina_de_Estudos.py
@@ -5,19 +5,26 @@ import numpy as np
 
 
 st.set_page_config(
-    page_title="Tempo de Tela",
+    page_title="Rotina de Estudos",
     page_icon="🔵",
 )
-
-q1 = st.select_slider(
-    "Sou pontual e frequente na escola?",
-    options=[
+st.image("https://www.colegiosantoantonio.com.br/wp-content/uploads/2024/06/csa-115-anos-1.png")
+st.title("Manhã do Conhecimento CSA")
+opt = [
         "Nunca",
         "Raramente",
         "Às vezes",
         "Quase sempre",
         "Sempre",
         
-    ],
-)
-st.write("My favorite color is", q1)
+    ]
+def pont(q):
+    pontuacao = opt.index(q)+1
+    return(q)
+
+st.header("Avaliando o estudo na sala de aula", divider=True)
+pont(st.select_slider(
+    "Sou pontual e frequente na escola?",
+    options=opt,
+))
+st.header(pont)
